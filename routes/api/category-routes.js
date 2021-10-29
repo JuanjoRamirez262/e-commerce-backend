e60f843a010fb9ec94394c34ed1c455f55b162dd
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-10-28 19:34:04 -0700

    routes working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,87 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    Category.findAll({
+      include: [Product]
+    }).then(categories => {
+      res.status(200).json(categories)
+    })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    Category.findByPk(req.params.id, {
+      include: [Product]
+    }).then(category => {
+      if (category) {
+        res.status(200).json(category)
+      } else {
+        res.status(404).json({ message: 'Category not found.' })
+      }
+    })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    Category.create({
+      category_name: req.body.category_name
+    }).then(newCategory => {
+      res.status(200).json({ newCategory })
+    })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    Category.update({
+      category_name: req.body.category_name
+    },
+      {
+        where: {
+          id: req.params.id
+        }
+      }).then(categoryUpdated => {
+        if (!categoryUpdated) {
+          res.status(404).json({ message: 'category not found.' })
+        } else {
+          res.status(200).json({ categoryUpdated })
+        }
+      })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(categoryDeleted => {
+      if (!categoryDeleted) {
+        return res.status(404).json({ message: 'Category not found' })
+      } else {
+        res.status(200).json({message: "Category deleted"})
+      }
+    })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,87 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    Tag.findAll({
+      include: [Product]
+    }).then(tags => {
+        res.status(200).json(tags)
+      })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    Tag.findByPk(req.params.id, {
+      include: [Product]
+    }).then(tags => {
+      if (tags) {
+        res.status(200).json(tags)
+      } else {
+        res.status(404).json({ message: 'Tag not found.' })
+      }
+    })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    Tag.create({
+      tag_name: req.body.tag_name
+    }).then(newtag => {
+      res.status(200).json({ newtag })
+    })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    Tag.update({
+      tag_name: req.body.tag_name
+    },
+      {
+        where: {
+          id: req.params.id
+        }
+      }).then(tagUpdated => {
+        if (!tagUpdated) {
+          res.status(404).json({ message: 'tag not found.' })
+        } else {
+          res.status(200).json({ tagUpdated })
+        }
+      })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(tagDeleted => {
+      if (!tagDeleted) {
+        return res.status(404).json({ message: 'Tag not found' })
+      } else {
+        res.status(200).json({message: "Tag deleted"})
+      }
+    })
+  } catch (err) {
+    res.status(500).json({ message: 'An error ocurred', err })
+  }
 });
 
 module.exports = router;
